5674d413b7c839ae3989325bd3b8dd977ed0e988
KarlMarxLendario <110194151+GabrielDz27@users.noreply.github.com>
2023-06-28 10:37:50 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
-import { AddContatoComponent } from './contato/add-contato/add-contato.component';
-import { ListContatoComponent } from './contato/list-contato/list-contato.component';
-import { EditContatoComponent } from './contato/edit-contato/edit-contato.component';
+
 import { AddBandaComponent } from './banda/add-banda/add-banda.component';
 import { ListBandaComponent } from './banda/list-banda/list-banda.component';
 import { EditbandaComponent } from './banda/edit-banda/edit-banda.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/list-contato', pathMatch: 'full' },
-  { path: 'add-contato', component: AddContatoComponent },
-  { path: 'list-contato', component: ListContatoComponent },
-  { path: 'edit-contato/:id', component: EditContatoComponent },
+
 
   { path: '', redirectTo: '/list-banda', pathMatch: 'full' },
   { path: 'add-banda', component: AddBandaComponent },
